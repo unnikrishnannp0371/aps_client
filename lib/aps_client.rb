1c5a34e4e3bf2034585452c708f6c2b5d861f24f
unnikrishnan <unnikrishnan.np@mgenindia.com>
2026-07-30 15:09:08 +0530

    Fix require paths for auth/ and aps/ subfolders

--- lib/aps_client.rb
+++ lib/aps_client.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 require_relative "aps_client/version"
 require_relative "aps_client/aps_errors"
-require_relative "aps_client/base"
+require_relative "aps_client/aps/base"
 require_relative "aps_client/application_service"
 require_relative "aps_client/aps_http"
-require_relative "aps_client/auth_service"
-require_relative "aps_client/data_management_service"
+require_relative "aps_client/auth/auth_service"
+require_relative "aps_client/aps/data_management_service"
 
 module ApsClient
   class Error < StandardError; end
